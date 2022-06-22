c7ea82766198040d22d4484f4607b79b521f5c69
srujan031 <76276571+srujan031@users.noreply.github.com>
2022-06-22 11:45:24 +0530

    Update fastapi_1.py

--- fastapi_1.py
+++ fastapi_1.py
@@ -1,15 +1,7 @@
-from typing import Union
-
-from fastapi import FastAPI
-
-app = FastAPI()
+   from fastapi import FastAPI
+    app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    @app.get("/")
+    async def home():
+        return {"message": "Hello World"}
